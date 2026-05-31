b39562b384c62b3885efa822a41ef443383cfd74
rinoa991 <2544026152@qq.com>
2026-05-31 16:31:57 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // 填入 Supabase 项目信息后，就会从本地模式切换成共享同步模式。
 // 这两个值可以在 Supabase Dashboard > Project Settings > API 里找到。
-window.DATE_BOOK_SUPABASE_URL = "https://mjnxataimyspytmzvgsn.supabase.co/rest/v1/";
+window.DATE_BOOK_SUPABASE_URL = "https://mjnxataimyspytmzvgsn.supabase.co";
 window.DATE_BOOK_SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1qbnhhdGFpbXlzcHl0bXp2Z3NuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODAyMDMwNzUsImV4cCI6MjA5NTc3OTA3NX0.6eljzWNDThpch83DOgYL488iPAFulzfgaXcKYk5QCG0";
